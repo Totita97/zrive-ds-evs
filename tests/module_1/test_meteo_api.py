21579d08382a3f02d3564d7d9b4f243d9e7cd209
Totita97 <evarodriguezborrajo@gmail.com>
2024-04-12 21:53:15 +0200

    Add unit test

--- tests/module_1/test_meteo_api.py
+++ tests/module_1/test_meteo_api.py
@@ -1,7 +1,14 @@
-""" This is a dummy example to show how to import code from src/ for testing"""
+import unittest
+from src.module_1.module_1_meteo_api import process_data
 
-from src.module_1.module_1_meteo_api import main
 
-
-def test_main():
-    raise NotImplementedError
+class TestProcessData(unittest.TestCase):
+    def runTest(self):
+        data = {
+            "time": ["2021-01-01", "2021-01-02"],
+            "temperature_2m_mean": [10, 20],
+            "precipitation_sum": [0, 5],
+            "soil_moisture_0_to_10cm_mean": [0.5, 0.6],
+        }
+        processed_data = process_data(data)
+        self.assertEqual(len(processed_data), 3)
